ec2a21b7b6c271c57522028525ffba633db50c40
Michael A. Schulze <mschulze@oxy.edu>
2014-11-18 11:00:02 -0800

    Making it easier to read
    Added spaces and lines to make it easier to read.

--- D&Ddice.py
+++ D&Ddice.py
@@ -4,34 +4,39 @@ restart = 1  #enterintoloop
 print "------------------------------------------------------------------------------"
 print "_________________________to end program enter 'off'___________________________"
 
-tracker = 0
+tracker = 0 #startstrackerat0
 
 while restart == 1:  #allowsforlooprestart
-	dice_type = raw_input("What type of dice do you want to roll?:")  #userinput
+    dice_type = raw_input("What type of dice do you want to roll?:")  #userinput
 
-	while True:  #generatesloop
+    while True:  #generatesloop
 		if dice_type == "d6" or dice_type == "6":  #readsuserinput
 			min = 1  #dicemin
 			max = 6  #dicemax
 			tracker += 1
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max) #randomgenerator
 		        if x == 1 or x == 2:
-		            print x
-		            print "well derp...."
-		            restart = 1
-		            break
+		            print x     #printrandomvalue
+		            print "well derp"   #printtextwhenroll1or2
+		            print "   "
+		            print "............................................."
+		            restart = 1 #resetsloop
+		            break   #endsloop
 		        elif x == 3 or x == 4:
 		            print x
-		            print "not too bad...."
+		            print "not too bad"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 5 or x == 6:
 		            print x
-		            print "nice roll...."
+		            print "nice roll"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
     		
@@ -39,25 +44,29 @@ while restart == 1:  #allowsforlooprestart
 			min = 1
 			max = 4
 			tracker += 1
-			print tracker
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max)
 		        if x == 1:
 		            print x
-		            print "well it's d4 what did you expect...."
+		            print "well it's d4 what did you expect"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 2 or x == 3:
 		            print x
-		            print "not too bad...."
+		            print "not too bad"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 4:
 		            print x
-		            print "nice roll...."
+		            print "nice roll"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
     	
@@ -65,25 +74,29 @@ while restart == 1:  #allowsforlooprestart
 			min = 1
 			max = 8
 			tracker += 1
-			print tracker
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max)
 		        if x == 1 or x == 2 or x == 3:
 		            print x
-		            print "well derp...."
+		            print "well derp"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 4 or x == 5:
 		            print x
-		            print "not too bad...."
+		            print "not too bad"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 6 or x == 7 or x == 8:
 		            print x
-		            print "nice roll...."
+		            print "nice roll"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
     				
@@ -91,30 +104,36 @@ while restart == 1:  #allowsforlooprestart
 			min = 1
 			max = 10
 			tracker += 1
-			print tracker
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max)
 		        if x == 1 or x == 2 or x == 3:
 		            print x
-		            print "well derp...."
+		            print "well derp"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 4 or x == 5 or x == 6:
 		            print x
-		            print "not too bad...."
+		            print "not too bad"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 7 or x == 8 or x == 9:
 		            print x
-		            print "nice roll...."
+		            print "nice roll"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 10:
 		            print x
-		            print "killin' it...."
+		            print "killin' it"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
     		
@@ -122,30 +141,36 @@ while restart == 1:  #allowsforlooprestart
 			min = 1
 			max = 12
 			tracker += 1
-			print tracker
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max)
 		        if x == 1 or x == 2 or x == 3:
 		            print x
-		            print "well derp...."
+		            print "well derp"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 4 or x == 5 or x == 6:
 		            print x
-		            print "not great...."
+		            print "not great"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 7 or x == 8 or x == 9:
 		            print x
-		            print "nice roll...."
+		            print "nice roll"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
     			elif x == 10 or x == 11 or x == 12:
     			    print x
-    			    print "dang...."
+    			    print "dang"
+    			    print "   "
+    			    print "............................................."
     			    restart = 1
     			    break
     			
@@ -153,45 +178,57 @@ while restart == 1:  #allowsforlooprestart
 			min = 1
 			max = 20
 			tracker += 1
-			print tracker
-			print ".............................."
 			print "dice roll # %s" % (tracker)
-			print "Fate rolls the dice..."
-			print "and the value is...."
+			print "Fate rolls the dice"
+			print "and the value is"
 			x = random.randint(min, max)
 		        if x == 1:
 		            print x
-		            print "natural fail...."
+		            print "natural fail"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 2 or x == 3 or x == 4 or x == 5:
 		            print x
-		            print "at least you didn't roll a 1...."
+		            print "at least it wasn't a 1"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 6 or x == 7 or x == 8 or x == 9:
 		            print x
-		            print "meh...."
+		            print "meh"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 10 or x == 11 or x == 12 or x == 13:
 		            print x
-		            print "not too shabby...."
+		            print "not too shabby"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 14 or x == 15 or x == 16 or x == 17:
 		            print x
-		            print "sweet roll dude...."
+		            print "sweet roll dude"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 18 or x == 19:
 		            print x
-		            print "ooo kill 'em...."
+		            print "ooo kill 'em"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 		        elif x == 20:
 		            print x
-		            print "natural crit...."
+		            print "natural crit"
+		            print "   "
+		            print "............................................."
 		            restart = 1
 		            break
 			
@@ -199,8 +236,10 @@ while restart == 1:  #allowsforlooprestart
 		    restart = 0
 		
 		else:  #unrelateduserinput
-			print "...."
-			print "we don't use that kind of dice in d&d"
-			print "...."
-			restart = 1
-			break
+		    print "Xx-----------------------------------------xX"
+		    print "    We don't use that kind of dice in d&d"
+		    print "Xx-----------------------------------------xX"
+		    print "   "
+		    print "............................................."
+		    restart = 1
+		    break
