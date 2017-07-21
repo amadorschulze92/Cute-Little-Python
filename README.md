2537a45a777c127b898e98ff3a5e997085bd199a
Michael Schulze <amadorschulze92@gmail.com>
2017-07-21 11:23:20 -0700

    rebirth

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ My Python Codes
 
 Some simple/early python codes I wrote for fun or to help me save time.
 
-  D&D code: Able to type in the type of dice you want to use and it rendomly generates a random number for that dice
+  D&D code: Able to type in the type of dice you want to use and it randomly generates a number for that dice
             type. It keeps track of the number of times you roll the dice. It prints a short textual response to the
             value of your roll. Then repeats loop.
             
--- webtest.py
+++ webtest.py
@@ -1,19 +1,26 @@
+#import time
 import socket
+import time
 import sys
 from urllib2 import urlopen, URLError, HTTPError
 
+#start_time = time.time()
 socket.setdefaulttimeout( 60 )  # timeout in seconds
 loop = 0
 count = 0
-url = 'https://github.com/search?o=desc&q=repos%3A%3E%3D1&ref=searchresults&s=&type=Users&utf8=%E2%9C%93'
+url = 'http://www.tripadvisor.com/Hotels-g28926-California-Hotels.html'
 
 def test(url, count, loop):
 	try :
 		response = urlopen( url )
 	except HTTPError, e:
+		main ()
+		#print("--- %s seconds ---" % (time.time() - start_time))
 		print 'Server couldn\'t fulfill request. Reason:', str(e.code)
 		sys.exit()
 	except URLError, e:
+		main ()
+		#print("--- %s seconds ---" % (time.time() - start_time))
 		print 'Failed to reach server. Reason:', str(e.reason)
 		sys.exit()
 	else :
@@ -22,5 +29,6 @@ def test(url, count, loop):
 		print count
     
 while loop == 0:
+	time.sleep(0) # sleep for x seconds
 	count += 1
 	test(url, count, loop)
